452ef4d6d9a4c6c41056eb02436d7c0b1ebafd80
Michael <michael@websr.eu>
2017-02-05 12:33:07 +0100

    Cron env Bugfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,14 @@
 set -e
 
 cat << EOF >> /etc/cron.d/backup
-${CRON} root /bin/bash -l -c "/backup.sh > /var/log/backup"
+${CRON} root . /root/backup_env.sh; /backup.sh > /var/log/backup
 
 EOF
 
 chmod 0644 /etc/cron.d/backup
+
+printenv | sed 's/^\(.*\)$/export \1/g' > /root/backup_env.sh
+chmod +x /root/backup_env.sh
 
 echo "Starting cron..."
 
